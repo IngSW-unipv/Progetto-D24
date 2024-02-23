0bdbd4bab338677782c7ba11e5fbcca4e284c911
xNichox <nicholas.schiavonravazz01@universitadipavia.it>
2024-02-23 15:31:28 +0100

    LocationStatsView Modificato

--- src/it/unipv/insfw23/TicketWave/modelView/LocationStatsView.java
+++ src/it/unipv/insfw23/TicketWave/modelView/LocationStatsView.java
@@ -1,22 +1,20 @@
 package it.unipv.insfw23.TicketWave.modelView;
 
+import it.unipv.insfw23.TicketWave.modelDomain.event.Genre;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.chart.BarChart;
-import javafx.scene.chart.CategoryAxis;
-import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.XYChart;
+import javafx.scene.chart.*;
 import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 
 public class LocationStatsView extends Scene {
-    private String genre;
+    private String event;
     private Button backButton;
-    private XYChart<String, Number> genreSerie;
+    private XYChart<String, Number> LocationSerie;
     private ArtistStatsView artistPane;
 
     public LocationStatsView() {
@@ -30,37 +28,31 @@ public class LocationStatsView extends Scene {
 
         layout.setStyle("-fx-background-color: rgb(27,84,161)");
 
-        final NumberAxis yAxis = new NumberAxis(0, 100, 10);
+        // Creazioni assi X e Y (scheletro)
         final CategoryAxis xAxis = new CategoryAxis();
+        final NumberAxis yAxis = new NumberAxis();
         xAxis.setLabel("Località");
         yAxis.setLabel("Eventi");
 
+        final LineChart<String, Number> lineChart = new LineChart<String, Number> (xAxis, yAxis);
+        lineChart.setStyle("-fx-bar-fill: #ea8928;");
+        lineChart.setTitle("Statistiche sugli eventi nelle varie località");
 
-        final BarChart<String, Number> barChart = new BarChart<>(xAxis, yAxis);
-        barChart.setStyle("-fx-bar-fill: #ea8928;");
-        barChart.setTitle("Statistiche sugli eventi nelle varie località");
+        XYChart.Series<String,Number> series = new XYChart.Series<>();
 
-        barChart.setCategoryGap(45);
-
-        XYChart.Series<String, Number> series = new XYChart.Series<>();
-
-        barChart.setLegendVisible(false);
+        lineChart.setLegendVisible(false);
 
         // Aggiunta dei dati alla serie
-        series.getData().add(new XYChart.Data<>("Categoria 1", 56));
-        series.getData().add(new XYChart.Data<>("Categoria 2", 66));
-        series.getData().add(new XYChart.Data<>("Categoria 3", 89));
-        series.getData().add(new XYChart.Data<>("Categoria 4", 12));
-        series.getData().add(new XYChart.Data<>("Categoria 5", 0));
+        series.getData().add(new XYChart.Data<>("Località 1", 100));
+        series.getData().add(new XYChart.Data<>("Località 2", 20));
+        series.getData().add(new XYChart.Data<>("Località 3", 30));
+        series.getData().add(new XYChart.Data<>("Località 4", 45));
+        series.getData().add(new XYChart.Data<>("Località 5", 73));
 
         // Aggiunta della serie al grafico
-        barChart.getData().add(series);
+        lineChart.getData().add(series);
 
-        for (Node n : barChart.lookupAll(".default-color0.chart-bar")) {
-            n.setStyle("-fx-bar-fill: rgba(238,109,33,0.99);");
-        }
-
-
+        // Back button
         Button backButton = new Button();
         ImageView backbuttonicon = new ImageView("it/unipv/insfw23/TicketWave/modelView/Resources/back2.png");
         backButton.setStyle("-fx-background-color: rgb(27,84,161)");
@@ -68,12 +60,11 @@ public class LocationStatsView extends Scene {
         backbuttonicon.setFitWidth(30);
 //      backButton.setGraphic(backbuttonicon);
 
-
+        // Pane che contiene il LineChart
         BorderPane paneGraph2 = new BorderPane();
         paneGraph2.setBackground((new Background(new BackgroundFill(Color.WHITE, new CornerRadii(10), Insets.EMPTY))));
         //paneGraph2.setPadding(new Insets(20));
-        paneGraph2.setCenter(barChart);
-
+        paneGraph2.setCenter(lineChart);
         paneGraph2.setMaxWidth(600);
         paneGraph2.setMaxHeight(400);
 
@@ -87,8 +78,8 @@ public class LocationStatsView extends Scene {
         contenuto.setPadding(new Insets(20));
 
         ArtistStatsView artistPane = new ArtistStatsView();
-        this.artistPane = artistPane;
-        contenuto.setRight(artistPane);
+        this.artistPane=artistPane;
+        contenuto.setLeft(artistPane); // o rimetto SetRight????
 
         BorderPane.setAlignment(backButton, Pos.TOP_RIGHT);
         BorderPane.setMargin(backButton, new Insets(10));
@@ -98,5 +89,29 @@ public class LocationStatsView extends Scene {
         layout.setCenter(contenuto);
         layout.setBottom(LowerBar.getInstance());
 
+        BorderPane.setAlignment(backButton, Pos.TOP_RIGHT);
+        BorderPane.setMargin(backButton, new Insets(10));
+        this.backButton = backButton;
+
+        layout.setTop(ManagerUpperBar.getIstance());
+        layout.setCenter(contenuto);
+        layout.setBottom(LowerBar.getInstance());
+
+    }
+
+    public XYChart<String, Number> getLocationSerie() {
+        return LocationSerie;
+    }
+
+    public void setEvent(String event) {
+        this.event = event;
+    }
+
+    public Button getBackButton(){
+        return backButton;
+    }
+
+    public ArtistStatsView getArtistPane() {
+        return artistPane;
     }
 }
