d155883473842cdf2aaf988b14792047292602ba
claud <claudio.cusumano01@universitadipavia.it>
2024-02-26 20:25:14 +0100

    Controller P Selection sistemato

--- src/it/unipv/insfw23/TicketWave/modelController/BuyTicketController.java
+++ src/it/unipv/insfw23/TicketWave/modelController/BuyTicketController.java
@@ -8,16 +8,20 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import it.unipv.insfw23.TicketWave.modelDomain.event.*;
 
 
 public class BuyTicketController {
     private Stage mainStage;
     private TicketPageView ticketPage;
 
+    private Event event;
 
-    public BuyTicketController(Stage mainStage,TicketPageView ticketPage ) {
+
+    public BuyTicketController(Stage mainStage,TicketPageView ticketPage, Event event) {
         this.mainStage = mainStage;
         this.ticketPage = ticketPage;
+        this.event=event;
         initComponents();
 
     }
