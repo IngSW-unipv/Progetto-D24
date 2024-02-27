ab0777f85c83303a35a154523e88c2226987b01d
xNichox <nicholas.schiavonravazz01@universitadipavia.it>
2024-02-27 15:39:44 +0100

    ProfileDAO + Tolte cose inutili in ResearchView + ResultResearchView

--- src/it/unipv/insfw23/TicketWave/Dao/ProfileDAO.java
+++ src/it/unipv/insfw23/TicketWave/Dao/ProfileDAO.java
@@ -1,7 +1,6 @@
 package it.unipv.insfw23.TicketWave.Dao;
 
-import it.unipv.insfw23.TicketWave.modelDomain.event.Event;
-import it.unipv.insfw23.TicketWave.modelDomain.event.Genre;
+import it.unipv.insfw23.TicketWave.modelDomain.event.*;
 import it.unipv.insfw23.TicketWave.modelDomain.user.Customer;
 import it.unipv.insfw23.TicketWave.modelDomain.user.Manager;
 import it.unipv.insfw23.TicketWave.modelDomain.user.User;
@@ -9,6 +8,7 @@ import it.unipv.insfw23.TicketWave.modelDomain.user.User;
 import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Objects;
 
 import it.unipv.insfw23.TicketWave.modelDomain.user.User;
 
@@ -120,9 +120,14 @@ public class ProfileDAO implements IProfileDAO{
 
             resultSet1 = statement1.executeQuery(query);
 
+
+
             if (resultSet1.next()) {
+                ArrayList<Event> event = new ArrayList<>();
+                event = getManagerEvent(resultSet1.getString("Event")); // passo la stringa dell'evento all metodo che mi restituisce un'ArrayList di eventi
+
                 manager = new Manager(resultSet1.getString("name"), resultSet1.getString("surname"), resultSet1.getString(3), resultSet1.getString(4),
-                                resultSet1.getString(5), resultSet1.getInt(6), resultSet1.getString(7), resultSet1.getString(8), resultSet1.getInt(9),
+                                resultSet1.getString(5), resultSet1.getInt(6), resultSet1.getString(7), event, resultSet1.getInt(9),
                                 resultSet1.getInt(10), resultSet1.getDate(11), resultSet1.getInt(12));
             }
 
@@ -165,5 +170,75 @@ public class ProfileDAO implements IProfileDAO{
     @Override
     public void setSubscription(Manager manager) {
 
+    }
+
+    public ArrayList<Event> getManagerEvent(String event) throws SQLException {
+        ResultSet resultSet2;
+        PreparedStatement statement2;
+        ArrayList<Event> result = new ArrayList<>();
+
+        if (!ConnectionDB.isOpen(conn)){ // checcko se la connessione al DB è chiusa, se è chiusa la riapro, altrimenti rifaccio la connessione al DB
+            conn = ConnectionDB.startConnection(conn,schema);
+        } else {
+            //ConnectionDB.closeConnection(conn);
+            //conn = ConnectionDB.startConnection(conn,schema);
+            String tipo;
+
+            String query1 = "SELECT ID_EVENT, TYPE FROM EVENT_ INNER JOIN MANAGER ON EVENT_.ID_MANAGER = MANAGER.MAIL WHERE ID_MANAGER = MAIL "; // prendi gli eventi di un certo manager e poi creagli l'arrayList
+
+            statement2 = conn.prepareStatement(query1);
+            resultSet2 = statement2.executeQuery(query1);
+
+            if (resultSet2.next()){
+                tipo = resultSet2.getString("TYPE");
+                event = resultSet2.getString("ID_EVENT");
+                int [] seatsRemainingForType = new int[2];
+                int [] priceForType = new int[2];
+                LocalDate ld = resultSet2.getDate("DATE").toLocalDate(); // converto la data in LocalDate
+                
+                int i = 0;
+                seatsRemainingForType[i] = resultSet2.getInt("REMAINING_BASE_SEATS"); // array di posti rimasti per tipo
+                seatsRemainingForType[i+1] = resultSet2.getInt("REMAINING_PREMIUM_SEATS");
+                seatsRemainingForType[i+1] = resultSet2.getInt("REMAINING_VIP_SEATS");
+                
+                i = 0;
+                priceForType[i] = resultSet2.getInt("BASE_PRICE"); // array dei prezzi rimasti per tipo
+                priceForType[i+1] = resultSet2.getInt("PREMIUM_PRICE");
+                priceForType[i+1] = resultSet2.getInt("VIP_PRICE");
+                
+                Event e;
+                switch (tipo) {
+                    case "FESTIVAL" -> {
+                        (Festival) e = new Festival(1, resultSet2.getString("NAME"), resultSet2.getString("CITY"), ld,
+                                resultSet2.getString("LOCATION"), Province.valueOf(resultSet2.getString("PROVINCE")), resultSet2.getInt("MAXNUMBEROFSEATS"),
+                                resultSet2.getInt("TYPEOFSEATS"), seatsRemainingForType, priceForType,
+                                Genre.valueOf(resultSet2.getString("GENRE")), null, null);
+                    }
+                    case "CONCERTO" -> {
+                        (Concert) e = new Concert(1, resultSet2.getString("NAME"), resultSet2.getString("CITY"), ld,
+                                resultSet2.getString("LOCATION"), Province.valueOf(resultSet2.getString("PROVINCE")), resultSet2.getInt("MAXNUMBEROFSEATS"),
+                                resultSet2.getInt("TYPEOFSEATS"), seatsRemainingForType, priceForType,
+                                Genre.valueOf(resultSet2.getString("GENRE")), null, null);
+                    }
+                    case "TEATRO" -> {
+                        (Theater) e = new Theater(1, resultSet2.getString("NAME"), resultSet2.getString("CITY"), ld,
+                                resultSet2.getString("LOCATION"), Province.valueOf(resultSet2.getString("PROVINCE")), resultSet2.getInt("MAXNUMBEROFSEATS"),
+                                resultSet2.getInt("TYPEOFSEATS"), seatsRemainingForType, priceForType,
+                                Genre.valueOf(resultSet2.getString("GENRE")), null, null);
+                    }
+                    case "ALTRO" -> {
+                        (Other) e = new Other(1, resultSet2.getString("NAME"), resultSet2.getString("CITY"), ld,
+                                resultSet2.getString("LOCATION"), Province.valueOf(resultSet2.getString("PROVINCE")), resultSet2.getInt("MAXNUMBEROFSEATS"),
+                                resultSet2.getInt("TYPEOFSEATS"), seatsRemainingForType, priceForType,
+                                Genre.valueOf(resultSet2.getString("GENRE")), null, null);
+                    }
+                }
+
+                for (int j = 0; j < resultSet2.getRow(); j++){ // for fino all'ultima riga del resultSet
+                    result.add(e); // non prende il valore interno dello switch case
+                }
+            }
+        }
+        return result;
     }
 }
