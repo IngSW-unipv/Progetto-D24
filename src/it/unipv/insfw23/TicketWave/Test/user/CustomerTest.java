3b814f8e75f223eeaa4c6fae0d963aaa4ef21da9
bejan <andriana.bejan01@universitadipavia.it>
2024-03-18 17:29:12 +0100

    customer test

--- src/it/unipv/insfw23/TicketWave/Test/user/CustomerTest.java
+++ src/it/unipv/insfw23/TicketWave/Test/user/CustomerTest.java
@@ -1,12 +1,10 @@
 package it.unipv.insfw23.TicketWave.Test.user;
-import it.unipv.insfw23.TicketWave.modelDomain.event.Concert;
-import it.unipv.insfw23.TicketWave.modelDomain.event.Event;
-import it.unipv.insfw23.TicketWave.modelDomain.event.Genre;
-import it.unipv.insfw23.TicketWave.modelDomain.event.Province;
+import it.unipv.insfw23.TicketWave.modelDomain.event.*;
 import it.unipv.insfw23.TicketWave.modelDomain.payment.IPaymentAdapter;
 import it.unipv.insfw23.TicketWave.modelDomain.ticket.Ticket;
 import it.unipv.insfw23.TicketWave.modelDomain.ticket.TicketType;
 import it.unipv.insfw23.TicketWave.modelDomain.user.Customer;
+import it.unipv.insfw23.TicketWave.modelDomain.user.Manager;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -21,6 +19,7 @@ public class CustomerTest {
     private ArrayList<Ticket> ticketsList= new ArrayList<>();
     private IPaymentAdapter pay;
     private Event event;
+    private Manager mg;
 
 
     private double points;
@@ -33,6 +32,13 @@ public class CustomerTest {
         Genre[] favoriteGenre= {Genre.EDM,Genre.HOUSE,Genre.POP};
         customer=new Customer("Mario","Rossi","2000-10-10","mariorossi@gmail.com","123",01,favoriteGenre);
 
+        mg = new Manager("Giorgio", "Mastrota", "1990-01-01", "giorgiom@example.com", "eminflex", 1, "1234567890123456",
+                events, 5, 1, LocalDate.now(), 0);
+        int [] a = {20};
+        int [] b = {2080};
+        double [] p = {150};
+        fs = new Festival(0, "Nameless", "Como", "Parco di Como", LocalDate.of(2024,4,20), Time.valueOf("14:04:00"), Province.COMO, Genre.EDM, Type.FESTIVAL, 3000,
+                1, a, b, p, mg, "Rooler, Salmo, Nello Taver", "Festival di musica EDM", 3);
     }
 
 
