ff37187a2be6510827834dc7e7e12f4895e4b5a2
xNichox <nicholas.schiavonravazz01@universitadipavia.it>
2024-03-18 09:12:56 +0100

    Creazione NotificationHandler + factory generica per tutti + Handler (prodotto) generico per tutti

--- src/it/unipv/insfw23/TicketWave/modelDomain/FactoryHandler/HandlerCreator.java
+++ src/it/unipv/insfw23/TicketWave/modelDomain/FactoryHandler/HandlerCreator.java
@@ -2,6 +2,6 @@ package it.unipv.insfw23.TicketWave.modelDomain.FactoryHandler;
 
 import it.unipv.insfw23.TicketWave.modelDomain.notifications.NotificationHandler;
 
-public abstract class HandlerCreator {
+public abstract class HandlerCreator { // classe astratta Factory
     public abstract Handler createHandler();
 }
--- src/it/unipv/insfw23/TicketWave/modelDomain/FactoryHandler/NotificationHandlerCreator.java
+++ src/it/unipv/insfw23/TicketWave/modelDomain/FactoryHandler/NotificationHandlerCreator.java
@@ -2,7 +2,7 @@ package it.unipv.insfw23.TicketWave.modelDomain.FactoryHandler;
 
 import it.unipv.insfw23.TicketWave.modelDomain.notifications.NotificationHandler;
 
-public class NotificationHandlerCreator extends HandlerCreator{
+public class NotificationHandlerCreator extends HandlerCreator{ // implementazione del Factory, cioè classe che crea un Handler
 
     @Override
     public Handler createHandler() {
