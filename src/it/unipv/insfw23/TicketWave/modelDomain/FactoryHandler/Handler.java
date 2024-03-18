86e3c219deb90978c64208284e7acc8b74d36390
xNichox <150830737+xNichox@users.noreply.github.com>
2024-03-18 09:33:21 +0100

    Delete src/it/unipv/insfw23/TicketWave/modelDomain/FactoryHandler directory

--- src/it/unipv/insfw23/TicketWave/modelDomain/FactoryHandler/Handler.java
+++
@@ -1,5 +0,0 @@
-package it.unipv.insfw23.TicketWave.modelDomain.FactoryHandler;
-
-public interface Handler { // prodotto del factory che deve essere implementato dai vari Handler
-    void handle();
-}
--- src/it/unipv/insfw23/TicketWave/modelDomain/FactoryHandler/HandlerCreator.java
+++
@@ -1,7 +0,0 @@
-package it.unipv.insfw23.TicketWave.modelDomain.FactoryHandler;
-
-import it.unipv.insfw23.TicketWave.modelDomain.notifications.NotificationHandler;
-
-public abstract class HandlerCreator { // classe astratta Factory
-    public abstract Handler createHandler();
-}
