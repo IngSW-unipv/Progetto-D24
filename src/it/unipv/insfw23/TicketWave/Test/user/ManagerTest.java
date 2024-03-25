fc4a4aec2cf4659566d1a4bee762106d87e5f860
claud <claudio.cusumano01@universitadipavia.it>
2024-03-26 00:34:15 +0100

    Sistemazioni classi payment e mastercard + test istanza Mastercard/Paypal Adapter

--- src/it/unipv/insfw23/TicketWave/Test/user/ManagerTest.java
+++ src/it/unipv/insfw23/TicketWave/Test/user/ManagerTest.java
@@ -8,7 +8,9 @@ import it.unipv.insfw23.TicketWave.modelDomain.user.Manager;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.sql.Time;
 import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
@@ -17,59 +19,127 @@ import static org.junit.Assert.assertThrows;
 
 public class ManagerTest {
 
-    private Manager manager, manager1;
+    private Manager manager, manager1,manager2;
     private ArrayList<Event> events = new ArrayList<>();
     private ArrayList<Event> events1 = new ArrayList<>();
-
+    private ArrayList<Event> events2 = new ArrayList<>();
+    private int[] seatsremainedfortypecorrectevent;
+    int[] ticketsoldfortypecorrectevent;
+    double[] pricecorrectevent;
     @Before
     public void setUp() {
         // Preparazione dei dati per i test
         manager = new Manager("Giorgio", "Mastrota", "1990-01-01", "giorgiom@example.com", "eminflex", 1, "1234567890123456", events, 5, 1, LocalDate.now(), 0);
         manager1 = new Manager("Valentino", "Chiaro", "2000-01-01", "valentino@gmail.com", "Cicalone", 3, "1111111111000000", events1, 5, -1, LocalDate.now(), 0);
+        manager2 = new Manager("Valentino", "Chiaro", "2000-01-01", "valentino@gmail.com", "Cicalone", 3, "1111111111000000", events2, 5, 1, LocalDate.now(), 8);
+        seatsremainedfortypecorrectevent = new int[]{60};
+        ticketsoldfortypecorrectevent = new int[]{15};
+        pricecorrectevent = new double[]{35.50};
     }
 
     @Test
-    public void testCreateFestival() throws Exception {
-        manager.createFestival(1, "Festival Test", "City Test", "Location Test", LocalDate.now(), null, Province.AGRIGENTO, Genre.ROCK, Type.FESTIVAL, 100, 1, new int[]{100}, new int[]{0}, new double[]{50.0}, manager, "Pino Daniele,Calcutta", "Description Test", 2);
-        assertEquals(1, manager.getEvent().size());
+    public void testCreateFestival() {
+        try {
+            manager.createFestival(1, "Festival Test", "City Test", "Location Test", LocalDate.now(), Time.valueOf(LocalTime.of(20, 30)), Province.AGRIGENTO, Genre.ROCK, Type.FESTIVAL, 100, 1, seatsremainedfortypecorrectevent, ticketsoldfortypecorrectevent, pricecorrectevent, manager, "Pino Daniele,Calcutta", "Description Test", 2);
+            assertEquals(1, manager.getEvent().size());
+        }
+        catch(Exception e){
+                // TODO: handle exception
+                System.out.println(e.getMessage());
+            }
+
+
+
+
     }
 
     @Test
-    public void testDontCreateFestival() {
-        Exception exception = assertThrows(Exception.class, () -> {
-            manager1.createFestival(1, "Festival Test", "City Test", "Location Test", LocalDate.now(), null, Province.AGRIGENTO, Genre.ROCK, Type.FESTIVAL, 100, 1, new int[]{100}, new int[]{0}, new double[]{50.0}, manager, "Pino Daniele,Calcutta", "Description Test", 2);
-        });
-        assertEquals("Impossibile Creare l'evento:Festival Test.Non puoi creare altri Eventi", exception.getMessage());
+    public void testDontCreateFestival() {  //caso limite utilizzo un manager che abbia un subscription non accettabile
+        try {
+            manager1.createFestival(1, "Festival Test", "City Test", "Location Test", LocalDate.now(), Time.valueOf(LocalTime.of(20, 30)), Province.AGRIGENTO, Genre.ROCK, Type.FESTIVAL, 100, 1, seatsremainedfortypecorrectevent, ticketsoldfortypecorrectevent, pricecorrectevent, manager, "Pino Daniele,Calcutta", "Description Test", 2);
+        } catch (Exception e) {
+
+            assertEquals("Impossibile Creare l'evento:Festival Test.Non puoi creare altri Eventi", e.getMessage());
+        }
+    }
+
+    @Test public void testDontCreateFestival2(){ // caso limite utilizzo un manager con numero di eventi creati maggiori al massimo
+        try {
+            manager2.createFestival(1, "Festival Test", "City Test", "Location Test", LocalDate.now(), Time.valueOf(LocalTime.of(20, 30)), Province.AGRIGENTO, Genre.ROCK, Type.FESTIVAL, 100, 1, seatsremainedfortypecorrectevent, ticketsoldfortypecorrectevent, pricecorrectevent, manager, "Pino Daniele,Calcutta", "Description Test", 2);
+        } catch (Exception e) {
+
+            assertEquals("Impossibile Creare l'evento:Festival Test.Non puoi creare altri Eventi", e.getMessage());
+        }
+
+    }
+
+    @Test public void testDontCreateFestival3(){ // caso limite utilizzo test con parametri nulli su data, non si può creare un evento con data nulla  DA SISTEMARE LO ISTANZIA ANCORA
+        try {
+            manager2.createFestival(1, null, "City Test", "Location Test", null,null, Province.AGRIGENTO, null, Type.FESTIVAL, 100, 1, seatsremainedfortypecorrectevent, ticketsoldfortypecorrectevent, pricecorrectevent, manager, "Pino Daniele,Calcutta", "Description Test", 2);
+            assertEquals(2, manager.getEvent().size());
+        }
+        catch(Exception e){
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
     }
 
 
 
 
+
+
+
     @Test
-    public void testCreateTheater() throws Exception {
-        manager.createTheater(3, "Theater Test", "City Test", "Location Test", LocalDate.now(), null, Province.AGRIGENTO, Genre.DRAMMA, 300, 1, new int[]{300}, new int[]{0}, new double[]{150.0}, manager, new ArrayList<>(), "Description Test", "Author Test");
-        assertEquals(1, manager.getEvent().size());
+    public void testCreateTheater() {
+        try {
+            manager.createTheater(3, "Theater Test", "City Test", "Location Test", LocalDate.now(), Time.valueOf(LocalTime.of(20, 30)), Province.AGRIGENTO, Genre.DRAMMA, Type.THEATER, 1, 1, seatsremainedfortypecorrectevent, ticketsoldfortypecorrectevent, pricecorrectevent, manager, "Checco Zalone,Jerry Calà", "Description Test", "Author Test");
+            assertEquals(1, manager.getEvent().size());
+        }
+        catch(Exception e){
+                // TODO: handle exception
+                System.out.println(e.getMessage());
+            }
+
     }
 
 
     @Test
-    public void testCreateConcert() throws Exception {
-        manager.createConcert(1, "Concert Test", "City Test", "Location Test", LocalDate.now(), null, Province.AGRIGENTO, Genre.POP, Type.CONCERT, 200, 1, new int[]{200}, new int[]{0}, new double[]{100.0}, manager, "Pino Daniele", "Description Test");
-        assertEquals(1, manager.getEvent().size());
+    public void testCreateConcert() {
+        try {
+            manager.createConcert(1, "Concert Test", "City Test", "Location Test", LocalDate.now(), Time.valueOf(LocalTime.of(20, 30)), Province.AGRIGENTO, Genre.POP, Type.CONCERT, 200, 1, seatsremainedfortypecorrectevent, ticketsoldfortypecorrectevent, pricecorrectevent, manager, "Pino Daniele", "Description Test");
+            assertEquals(1, manager.getEvent().size());
+        }
+        catch(Exception e){
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
     }
 
     @Test
-    public void testCreateOther() throws Exception {
-        manager.createOther(4, "Other Test", "City Test", "Location Test", LocalDate.now(), null, Province.AGRIGENTO, Genre.ROCK, Type.OTHER, 400, 1, new int[]{400}, new int[]{0}, new double[]{200.0}, manager, "Comico", "Description Test");
-        assertEquals(1, manager.getEvent().size());
+    public void testCreateOther()  {
+        try {
+            manager.createOther(4, "Other Test", "City Test", "Location Test", LocalDate.now(), Time.valueOf(LocalTime.of(20, 30)), Province.AGRIGENTO, Genre.ROCK, Type.OTHER, 400, 1, seatsremainedfortypecorrectevent, ticketsoldfortypecorrectevent, pricecorrectevent, manager, "Comico", "Description Test");
+            assertEquals(1, manager.getEvent().size());
+        }
+        catch(Exception e){
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
     }
 
 
 
     @Test
     public void testOneMonthPassed() {
-        boolean result = manager.OneMonthPassed();
-        assertFalse(result); // Deve essere falso, poiché non è passato un mese
+        try {
+            boolean result = manager.OneMonthPassed();
+            assertFalse(result); // Deve essere falso, poiché non è passato un mese
+        }
+        catch(Exception e){
+            // TODO: handle exception
+            System.out.println(e.getMessage());
+        }
     }
 
 
