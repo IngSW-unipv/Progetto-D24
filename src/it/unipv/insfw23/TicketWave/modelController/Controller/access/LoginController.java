b97a05f363ae7d59c1f36749e9ffd64a3ab7777c
bejan <andriana.bejan01@universitadipavia.it>
2024-04-17 15:23:56 +0200

    aggiustamento colori e font

---
+++ src/it/unipv/insfw23/TicketWave/modelController/Controller/access/LoginController.java
@@ -0,0 +1,130 @@
+package it.unipv.insfw23.TicketWave.modelController.Controller.access;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import it.unipv.insfw23.TicketWave.modelController.Controller.user.CustomerController;
+import it.unipv.insfw23.TicketWave.modelDomain.event.Event;
+import it.unipv.insfw23.TicketWave.modelDomain.event.Genre;
+import it.unipv.insfw23.TicketWave.modelDomain.event.Province;
+import it.unipv.insfw23.TicketWave.modelDomain.user.Manager;
+import it.unipv.insfw23.TicketWave.modelView.access.LoginView;
+import it.unipv.insfw23.TicketWave.modelView.access.SignUpView;
+import it.unipv.insfw23.TicketWave.modelView.user.CustomerView;
+import javafx.event.*;
+import javafx.stage.Stage;
+public class LoginController {
+
+    private Stage mainstage;
+
+    // view da considerare
+    private SignUpView signUpView;
+    private CustomerView customerview;
+    private LoginView loginView;
+    private ManagerView managerView;
+
+
+    public LoginController(Stage mainstage, SignUpView signUpView, CustomerView customerview, LoginView loginView,ManagerView managerView) {
+        this.mainstage = mainstage;
+        this.signUpView = signUpView;
+        this.customerview = customerview;
+        this.loginView = loginView;
+        this.managerView=managerView;
+
+        initComponents();
+    }
+
+    public void initComponents() {
+
+
+        EventHandler<ActionEvent> goToSignUpView = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                // Azione da eseguire quando il pulsante "Registrati" viene premuto
+                System.out.println("Hai cliccato il pulsante Registrati");
+                signUpView = new SignUpView();
+                SignUpController signUpController = new SignUpController(mainstage, signUpView, customerview, loginView);
+                signUpView.reSetBars();
+                mainstage.setScene(signUpView); // Imposta la scena SignUpView sulla stage principale
+            }
+        };
+
+        loginView.getRegButton().setOnAction(goToSignUpView); // Imposta l'handler sull'azione del pulsante "Registrati"
+
+        EventHandler<ActionEvent> goToCustomerorManagerView = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                if (loginView.getCustomerRadioButton().isSelected()) {
+
+                    /*creazione customer ed evento per poi creare vari biglietti e fare delle verifiche
+                     *
+                     * */
+
+
+                    System.out.println("Hai cliccato il pulsante Login come cliente");
+                    customerview = new CustomerView();
+                    CustomerController customerController = new CustomerController(mainstage,customerview,loginView);
+                    customerview.reSetBars();
+                    mainstage.setScene(customerview); // Imposta la scena SignUpView sulla stage principale
+                }
+                else if (loginView.getManagerRadioButton().isSelected()) {
+
+                    /*	modifiche fatte da Loris per simulare la creazione di un manager senza eventi, simulare la creazione di un evento
+                     * 	da parte di questo manager e simulare aggiunta di questo evento al suo arraylist di eventi
+                     * 	cosi da lavorare con un ipotetico manager che ha pubblicato degli eventi
+                     * */
+
+
+
+                    ArrayList<Event> arraylistevent = new ArrayList<>();
+                    LocalDate datasub = LocalDate.of(2024, 02, 25);
+                    Manager managerfinto = new Manager("paolo","brosio","2000-12-30","paobro@gmail.com","password1234",Province.CAGLIARI.ordinal(), "2324523432451420", arraylistevent,5,1,datasub,0);
+
+                    int[] seduteRImasteev1 = {200,100};
+                    double[] vettfalsopriceev1 = {5,320};
+                    LocalDate dataev1 = LocalDate.of(2025, 03, 20);
+                    ArrayList<String> arrfintoartista1 = new ArrayList<>();
+                    arrfintoartista1.add("califano");
+                    //Concert eventofinto1 = new Concert(12,"reunion","busto arstizio",dataev1, "via dei matti ,0", Province.LIVORNO,300,2, seduteRImasteev1, vettfalsopriceev1,Genre.INDIE,managerfinto,arrfintoartista1);
+
+                    int[] seduterimasteev2 = {100,80,12};
+                    double[] vettfalsopriceev2 = {11,250,500};
+                    LocalDate dataev2 = LocalDate.of(2024, 10, 02);
+                    ArrayList<String> arrfintoartista2 = new ArrayList<>();
+                    arrfintoartista2.add("loredana berte");
+                    //Concert eventofinto2 = new Concert(12,"reunion","busto arstizio",dataev2, "via dei matti ,0", Province.LIVORNO,300,2, seduterimasteev2, vettfalsopriceev2,Genre.INDIE,managerfinto,arrfintoartista2);
+
+                    try {
+                        managerfinto.createConcert(12, "reunion", "busto arstizio", dataev1, "via dei matti ,o", Province.LIVORNO, 300, 2, seduteRImasteev1, vettfalsopriceev1, Genre.INDIE, managerfinto, arrfintoartista1);
+                        managerfinto.createConcert(14, "festino", "Rozzano", dataev2, "stadio dell'albero", Province.MILANO, 10000, 3, seduterimasteev2, vettfalsopriceev2, Genre.HOUSE, managerfinto, arrfintoartista2);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    /*
+                     * 	fine modifiche di Loris
+                     * */
+
+
+                    // Azione da eseguire quando il pulsante "Login" viene premuto
+                    System.out.println("Hai cliccato il pulsante Login come gestore");
+                    managerView = new ManagerView();
+                    managerView.setEventsforTableev(managerfinto);
+                    ManagerController managerController = new ManagerController(mainstage, managerView, loginView, managerfinto);
+                    managerView.reSetBars();
+                    mainstage.setScene(managerView); // Imposta la scena SignUpView sulla stage principale }
+                }// devi mettere managerview anche all'interno del costruttore
+
+
+
+                // Imposta l'handler sull'azione del pulsante "Registrati"
+            }
+
+        };
+
+        loginView.getLoginButton().setOnAction(goToCustomerorManagerView);
+    }
+
+}
---
+++ src/it/unipv/insfw23/TicketWave/modelController/Controller/access/SignUpController.java
@@ -0,0 +1,80 @@
+package it.unipv.insfw23.TicketWave.modelController.Controller.access;
+
+import it.unipv.insfw23.TicketWave.modelController.Controller.user.CustomerController;
+import it.unipv.insfw23.TicketWave.modelView.access.LoginView;
+import it.unipv.insfw23.TicketWave.modelView.access.SignUpView;
+import it.unipv.insfw23.TicketWave.modelView.user.CustomerView;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.stage.Stage;
+
+public class SignUpController {
+    private Stage mainstage;
+
+    // view da considerare
+
+    private SignUpView signUpView;
+    private ManagerView managerView;
+    private CustomerView customerview;
+    private LoginView loginView;
+    private SubscriptionSelectionView subscriptionSelectionView;
+    private PaymentSelectionView paymentSelectionView;
+
+
+    public SignUpController(Stage mainstage, SignUpView signUpView, CustomerView customerview, LoginView loginView) {
+
+        this.signUpView = signUpView;
+        this.mainstage = mainstage;
+        this.customerview = customerview;
+        this.loginView = loginView;
+        // this.subscriptionSelectionView= subscriptionSelectionView;
+        initComponents();
+
+    }
+
+    public void initComponents() {
+
+        EventHandler<ActionEvent> goToLoginView = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                // Azione da eseguire quando il pulsante "Torna Indietro" viene premuto
+                System.out.println("Hai cliccato il pulsante Torna Indietro");
+                //loginView= new LoginView();
+                //LoginController loginController= new LoginController(mainstage,signUpView,customerview,loginView);
+                loginView.reSetBars();
+                mainstage.setScene(loginView.getScene()); // Imposta la scena LoginView sullo Stage principal
+
+            }
+        };
+
+        signUpView.getBackButton().setOnAction(goToLoginView);
+
+        EventHandler<ActionEvent> goToSelection = new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+
+                if (signUpView.getCustomerRadioButton().isSelected()) {
+
+
+                    System.out.println("Hai cliccato il pulsante registrati  come cliente");
+                    customerview = new CustomerView();
+                    CustomerController customerController = new CustomerController(mainstage,customerview,loginView);
+                    customerview.reSetBars();
+                    mainstage.setScene(customerview);
+                    // Imposta la scena SignUpView sulla stage principale
+                } else if (signUpView.getManagerRadioButton().isSelected()) {
+
+
+                    System.out.println("Hai cliccato il pulsante registrati  come gestore");
+                    subscriptionSelectionView = new SubscriptionSelectionView();
+                    SubscriptionSelectionController subscriptionSelectionController = new SubscriptionSelectionController(mainstage,subscriptionSelectionView,paymentSelectionView);
+
+                    subscriptionSelectionView.reSetBars();
+                    mainstage.setScene(subscriptionSelectionView);
+                    // Imposta la scena subscriptio sulla stage principale
+                }
+            }
+        };
+        signUpView.getSignUpButton().setOnAction(goToSelection);
+    }
+}
