da6f5fde7de6239cd3dc1da079b5d9afc32937da
xNichox <nicholas.schiavonravazz01@universitadipavia.it>
2024-04-21 14:11:10 +0200

    Modifiche riguardo l'attributo ProvinceOfResidence, l'ho messo di tipo Province e non int

--- src/it/unipv/insfw23/TicketWave/modelView/research/StarterNicholas.java
+++ src/it/unipv/insfw23/TicketWave/modelView/research/StarterNicholas.java
@@ -46,6 +46,7 @@ public class StarterNicholas extends Application {
          */
         UpperBarController ctrl = new UpperBarController(primaryStage);
         ResearchView scene = new ResearchView();
+        scene.getStylesheets().add("it/unipv/insfw23/TicketWave/modelView/tableViewStyle.css");
         ResearchController controller = new ResearchController(primaryStage, scene);
         primaryStage.setScene(scene);
 
