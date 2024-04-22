d32adcaa8b9ef8f9c91bbc7a2f5921cb8318d849
claud <claudio.cusumano01@universitadipavia.it>
2024-04-22 16:10:21 +0200

    Aggiunta di tickepage view+ classe di prova run

--- src/it/unipv/insfw23/TicketWave/modelView/subscription/SubscriptionSelectionView.java
+++ src/it/unipv/insfw23/TicketWave/modelView/subscription/SubscriptionSelectionView.java
@@ -1,33 +1,17 @@
 package it.unipv.insfw23.TicketWave.modelView.subscription;
 
-<<<<<<< HEAD
 import it.unipv.insfw23.TicketWave.modelView.bars.LowerBar;
 import it.unipv.insfw23.TicketWave.modelView.bars.UpperBar;
-=======
-
-
-import it.unipv.insfw23.TicketWave.modelView.bars.LowerBar;
-import it.unipv.insfw23.TicketWave.modelView.bars.UpperBar;
-import javafx.application.Application;
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-<<<<<<< HEAD
-=======
-import javafx.scene.image.Image;
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-<<<<<<< HEAD
-=======
-import javafx.stage.Stage;
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
 
 public class SubscriptionSelectionView extends Scene {
 
@@ -45,7 +29,6 @@ public class SubscriptionSelectionView extends Scene {
     public void init(){
 
         BorderPane contenuto = new BorderPane();
-<<<<<<< HEAD
        contenuto.setStyle("-fx-background-color: #def1fa;");
 
         BorderPane primaSub = new BorderPane();
@@ -54,16 +37,6 @@ public class SubscriptionSelectionView extends Scene {
         secondaSub.setStyle("-fx-background-color: rgb(103,186,255); -fx-background-radius: 20;");
         BorderPane terzaSub = new BorderPane();
         terzaSub.setStyle("-fx-background-color: rgba(103,186,255); -fx-background-radius: 20;");
-=======
-        contenuto.setStyle("-fx-background-color: #91bad6;");
-
-        BorderPane primaSub = new BorderPane();
-        primaSub.setStyle("-fx-background-color: #2E5984; -fx-background-radius: 20;");
-        BorderPane secondaSub = new BorderPane();
-        secondaSub.setStyle("-fx-background-color: #2E5984; -fx-background-radius: 20;");
-        BorderPane terzaSub = new BorderPane();
-        terzaSub.setStyle("-fx-background-color: #2E5984; -fx-background-radius: 20;");
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
 
         BorderPane.setMargin(primaSub, new Insets(30));
         BorderPane.setMargin(secondaSub, new Insets(30));
@@ -74,84 +47,48 @@ public class SubscriptionSelectionView extends Scene {
 
 
         Label nome1 = new Label("Nessun Abbonamento");
-<<<<<<< HEAD
         nome1.setFont(Font.font("Arial Rounded MT Bold", FontWeight.EXTRA_BOLD, 20));
-=======
-        nome1.setFont(Font.font("Helvetica", FontWeight.EXTRA_BOLD, 20));
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
         nome1.setTextFill(Color.BLACK);
         primaSub.setTop(nome1);
 
         Label descr1 = new Label("Descrizione 1");
-<<<<<<< HEAD
         descr1.setFont(Font.font("Arial Rounded MT Bold", FontWeight.EXTRA_BOLD, 20));
-=======
-        descr1.setFont(Font.font("Helvetica", FontWeight.EXTRA_BOLD, 20));
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
         descr1.setTextFill(Color.BLACK);
         primaSub.setCenter(descr1);
 
         Label prezzo1 = new Label("0,00 $");
-<<<<<<< HEAD
         prezzo1.setFont(Font.font("Arial Rounded MT Bold", FontWeight.EXTRA_BOLD, 20));
-=======
-        prezzo1.setFont(Font.font("Helvetica", FontWeight.EXTRA_BOLD, 20));
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
         prezzo1.setTextFill(Color.BLACK);
         primaSub.setBottom(prezzo1);
 
         Label nome2 = new Label("Abbonamento Base");
-<<<<<<< HEAD
         nome2.setFont(Font.font("Arial Rounded MT Bold", FontWeight.EXTRA_BOLD, 20));
-=======
-        nome2.setFont(Font.font("Helvetica", FontWeight.EXTRA_BOLD, 20));
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
         nome2.setTextFill(Color.BLACK);
         secondaSub.setTop(nome2);
 
         Label descr2 = new Label("Descrizione 2");
-<<<<<<< HEAD
         descr2.setFont(Font.font("Arial Rounded MT Bold", FontWeight.EXTRA_BOLD, 20));
-=======
-        descr2.setFont(Font.font("Helvetica", FontWeight.EXTRA_BOLD, 20));
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
         descr2.setTextFill(Color.BLACK);
         secondaSub.setCenter(descr2);
 
         Label prezzo2 = new Label("8,99 $");
-<<<<<<< HEAD
         prezzo2.setFont(Font.font("Arial Rounded MT Bold", FontWeight.EXTRA_BOLD, 20));
-=======
-        prezzo2.setFont(Font.font("Helvetica", FontWeight.EXTRA_BOLD, 20));
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
         prezzo2.setTextFill(Color.BLACK);
         secondaSub.setBottom(prezzo2);
 
 
         Label nome3 = new Label("   Abbonamento Premium");
-<<<<<<< HEAD
         nome3.setFont(Font.font("Arial Rounded MT Bold", FontWeight.EXTRA_BOLD, 20));
-=======
-        nome3.setFont(Font.font("Helvetica", FontWeight.EXTRA_BOLD, 20));
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
         nome3.setTextFill(Color.BLACK);
         terzaSub.setTop(nome3);
 
         Label descr3 = new Label("Descrizione 3");
-<<<<<<< HEAD
         descr3.setFont(Font.font("Arial Rounded MT Bold", FontWeight.EXTRA_BOLD, 20));
-=======
-        descr3.setFont(Font.font("Helvetica", FontWeight.EXTRA_BOLD, 20));
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
         descr3.setTextFill(Color.BLACK);
         terzaSub.setCenter(descr3);
 
         Label prezzo3 = new Label("14,99 $");
-<<<<<<< HEAD
         prezzo3.setFont(Font.font("Arial Rounded MT Bold", FontWeight.EXTRA_BOLD, 20));
-=======
-        prezzo3.setFont(Font.font("Helvetica", FontWeight.EXTRA_BOLD, 20));
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
         prezzo3.setTextFill(Color.BLACK);
         terzaSub.setBottom(prezzo3);
 
@@ -175,15 +112,9 @@ public class SubscriptionSelectionView extends Scene {
         secondaSub.setMaxSize(300, 250);
         terzaSub.setMaxSize(300, 250);
 
-<<<<<<< HEAD
        bottonePrimaSub = new StackPane(primaSub);
        bottoneSecondaSub = new StackPane(secondaSub);
        bottoneTerzaSub = new StackPane(terzaSub);
-=======
-        bottonePrimaSub = new StackPane(primaSub);
-        bottoneSecondaSub = new StackPane(secondaSub);
-        bottoneTerzaSub = new StackPane(terzaSub);
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
 
         bottonePrimaSub.setPadding(new Insets(20));
         bottoneSecondaSub.setPadding(new Insets(20));
@@ -216,10 +147,7 @@ public class SubscriptionSelectionView extends Scene {
 
         BorderPane temp = new BorderPane();
         setRoot(temp);
-<<<<<<< HEAD
         UpperBar.getIstance().setForManager();
-=======
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
         layout.setTop(UpperBar.getIstance());
         layout.setCenter(contenuto);
         layout.setBottom(LowerBar.getInstance());
@@ -239,11 +167,7 @@ public class SubscriptionSelectionView extends Scene {
         return bottoneTerzaSub;
     }
 
-<<<<<<< HEAD
 }
 
 
 
-=======
-}
->>>>>>> 24481fa7b4e4944187c3a3d0b9f4b46bd9ebd956
