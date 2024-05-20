1c32fc55517f2c78fa47f764922743e6b8e8ffdb
Davide-h <davide.colli03@universitadipavia.it>
2024-05-20 12:20:03 +0200

    Login manager fix

--- src/it/unipv/insfw23/TicketWave/modelController/controller/access/LoginController.java
+++ src/it/unipv/insfw23/TicketWave/modelController/controller/access/LoginController.java
@@ -134,7 +134,7 @@ public class LoginController {
                     arrayListNotification = managerfinto.getNotification();
                     managerView = new ManagerView("Marco",arrayListNotification,arraylistevent);
                     //managerView.setEventsforTableev(managerfinto);
-                    ManagerController managerController = new ManagerController(mainstage, managerView, loginView);
+
                     //managerView.reSetBars();
                     //ATTENZIONE, QUI VA LA CHIAMATA AL DAO
                     //
@@ -142,7 +142,7 @@ public class LoginController {
                     ConnectedUser.getInstance().setHome(managerView);
                     ConnectedUser.getInstance().setLoginView(loginView);
                     //
-
+                    ManagerController managerController = new ManagerController(mainstage, managerView, loginView);
                     mainstage.setScene(managerView); // Imposta la scena SignUpView sulla stage principale }
                 }// devi mettere managerview anche all'interno del costruttore
 
