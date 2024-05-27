81748ab314413b3558e0a19d729dd793778e0cf8
claud <claudio.cusumano01@universitadipavia.it>
2024-05-27 20:57:53 +0200

    insertManager+ insert Customer 1.0

--- src/it/unipv/insfw23/TicketWave/dao/profileDao/IProfileDao.java
+++ src/it/unipv/insfw23/TicketWave/dao/profileDao/IProfileDao.java
@@ -3,9 +3,12 @@ package it.unipv.insfw23.TicketWave.dao.profileDao;
 import it.unipv.insfw23.TicketWave.modelDomain.user.Customer;
 import it.unipv.insfw23.TicketWave.modelDomain.user.Manager;
 
+import java.sql.SQLException;
+
 public interface IProfileDao {
-    public void insertManager();
-    public void insertCustomer();
+    void insertManager(Manager manager) throws SQLException;
+
+    public void insertCustomer(Customer customer) throws SQLException;
     public Manager selectManager();
     public Customer selectCustomer();
 }
