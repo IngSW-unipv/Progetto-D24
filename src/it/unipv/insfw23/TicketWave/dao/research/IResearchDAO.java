409d0ce6f4a8f7b76073d2dc032945fbbb03a14f
xNichox <nicholas.schiavonravazz01@universitadipavia.it>
2024-05-29 09:15:29 +0200

    Continuo ResearchDAO

--- src/it/unipv/insfw23/TicketWave/dao/research/IResearchDAO.java
+++ src/it/unipv/insfw23/TicketWave/dao/research/IResearchDAO.java
@@ -6,10 +6,11 @@ import it.unipv.insfw23.TicketWave.modelDomain.event.Province;
 import it.unipv.insfw23.TicketWave.modelDomain.user.Manager;
 import it.unipv.insfw23.TicketWave.modelDomain.user.User;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
 public interface IResearchDAO {
-    public ArrayList<Event> getFilteredEvents(String search); // metodo della ricerca, esso fornisce i risultati
-    public ArrayList<Event> getAllEvents(); // Restituisco tutti gli eventi quando sul textField della ricerca non c'è nulla
+    public ArrayList<Event> getFilteredEvents(String search) throws SQLException; // metodo della ricerca, esso fornisce i risultati
+    public ArrayList<Event> getAllEvents() throws SQLException; // Restituisco tutti gli eventi quando sul textField della ricerca non c'è nulla
 }
--- src/it/unipv/insfw23/TicketWave/dao/research/ResearchDAO.java
+++ src/it/unipv/insfw23/TicketWave/dao/research/ResearchDAO.java
@@ -16,31 +16,11 @@ public class ResearchDAO implements IResearchDAO{
 
     public ResearchDAO() { // è un Object easy
         super();
-    }
+    } // costruttore
 
     @Override
-    public ArrayList<Event> getAllEvents() {
+    public ArrayList<Event> getAllEvents() throws SQLException{
         conn = ConnectionDB.startConnection(conn,schema);
-        PreparedStatement ps;
-        ResultSet rs;
-        ArrayList<Event> result = new ArrayList<>();
-
-        try{
-            result = getAllEventsFromDB(); // entro nel metodo che istanzia gli eventi
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        ConnectionDB.closeConnection(conn);
-        return result;
-    } // Quando sulla ResearchBar non ho nulla, allora restituisco tutti gli eventi
-    @Override
-    public ArrayList<Event> getFilteredEvents(String search) { // Quando qualcuno scrive sulla ResearchBar (TextField) e usa o meno i filtri, allora uso questo metodo
-
-        return null;
-    }
-
-    // Caso click sulla lente di ingrandimento senza aver ricercato nulla
-    private ArrayList<Event> getAllEventsFromDB() throws SQLException {
         ArrayList<Event> result = new ArrayList<>();
         Manager manager = null;
         PreparedStatement statement1;
@@ -48,81 +28,83 @@ public class ResearchDAO implements IResearchDAO{
         ResultSet resultset1;
         ResultSet resultset2;
 
-    try {
-        if (!ConnectionDB.isOpen(conn)) { // se non è aperta una connessione, allora aprila
-            ConnectionDB.startConnection(conn, schema);
-        }
-        String query = "SELECT * FROM EVENT_ WHERE MANAGER_ID = ?";
-        statement1 = conn.prepareStatement(query);
-        resultset1 = statement1.executeQuery(query);
-
-        while (resultset1.next()) {
-            LocalDate ld = resultset1.getDate("DATE_").toLocalDate();
-            LocalTime tm = resultset1.getTime("TIME_").toLocalTime();
-            Blob bl = resultset1.getBlob("PHOTO");
-            try {
-                String query2 = "SELECT * FROM MANAGER WHERE MAIL = ?";
-                statement2 = conn.prepareStatement(query2);
-                resultset2 = statement2.executeQuery(query2);
-                LocalDate ld2 = resultset2.getDate("SUBSCRIPTION_DATE").toLocalDate();
-
-                if (resultset2.next()) {
-                    manager = new Manager(resultset2.getString("NAME"), resultset2.getString("SURNAME"), resultset2.getDate("BIRTHDATE").toString(),
-                            resultset2.getString("MAIL"), resultset2.getString("PWD"), Province.valueOf(resultset1.getString("PROVINCE")),
-                            resultset2.getString("CARDNUMBER"), result, resultset2.getInt("MAXEVENTS"), resultset2.getInt("SUBSCRIPTION"),
-                            ld2, resultset2.getInt("COUNTER_CREATED_EVENTS"));
-                }
-
-            } catch (Exception e) {
-                e.printStackTrace();
+        try {
+            if (!ConnectionDB.isOpen(conn)) { // se non è aperta una connessione, allora aprila
+                ConnectionDB.startConnection(conn, schema);
             }
-            double[] price = {resultset1.getDouble("BASE_PRICE"), resultset1.getDouble("PREMIUM_PRICE"), resultset1.getDouble("VIP_PRICE")};
-            int[] seatsremaining = {resultset1.getInt("REMAINING_BASE_SEATS"), resultset1.getInt("REMAINING_PREMIUM_SEATS"), resultset1.getInt("REMAINING_VIP_SEATS")};
-            int[] ticketSoldNumberForType = {resultset1.getInt("SOLD_BASE_SEATS"), resultset1.getInt("SOLD_PREMIUM_SEATS"), resultset1.getInt("SOLD_VIP_SEATS")};
-            switch (resultset1.getInt("TYPE")) {
-                case 0 -> {
-                    Concert currentConcert = new Concert(resultset1.getInt("ID_EVENT"), resultset1.getString("NAME_"),
-                            resultset1.getString("CITY"), resultset1.getString("LOCATION"), ld, tm,
-                            Province.valueOf(resultset1.getString("PROVINCE")), Genre.valueOf(resultset1.getString("GENRE")),
-                            resultset1.getInt("MAX_NUM_SEATS"), resultset1.getInt("NUM_SEATS_TYPE"), seatsremaining, ticketSoldNumberForType, price,
-                            manager, resultset1.getString("ARTISTS"), resultset1.getString("DESCRIPTION_"), bl);
-                    result.add(currentConcert);
-                    break;
+            String query = "SELECT * FROM EVENT_ WHERE MANAGER_ID = ?";
+            statement1 = conn.prepareStatement(query);
+            resultset1 = statement1.executeQuery(query);
+
+            while (resultset1.next()) {
+                LocalDate ld = resultset1.getDate("DATE_").toLocalDate();
+                LocalTime tm = resultset1.getTime("TIME_").toLocalTime();
+                Blob bl = resultset1.getBlob("PHOTO");
+                try {
+                    String query2 = "SELECT * FROM MANAGER WHERE MAIL = ?";
+                    statement2 = conn.prepareStatement(query2);
+                    resultset2 = statement2.executeQuery(query2);
+                    LocalDate ld2 = resultset2.getDate("SUBSCRIPTION_DATE").toLocalDate();
+
+                    if (resultset2.next()) {
+                        manager = new Manager(resultset2.getString("NAME"), resultset2.getString("SURNAME"), resultset2.getDate("BIRTHDATE").toString(),
+                                resultset2.getString("MAIL"), resultset2.getString("PWD"), Province.valueOf(resultset1.getString("PROVINCE")),
+                                resultset2.getString("CARDNUMBER"), result, resultset2.getInt("MAXEVENTS"), resultset2.getInt("SUBSCRIPTION"),
+                                ld2, resultset2.getInt("COUNTER_CREATED_EVENTS"));
+                    }
+
+                } catch (SQLException e) {
+                    throw new RuntimeException("Errore nella query del manager (ResearchDAO)");
                 }
-                case 1 -> {
-                    Festival currentFestival = new Festival(resultset1.getInt("ID_EVENT"), resultset1.getString("NAME_"),
-                            resultset1.getString("CITY"), resultset1.getString("LOCATION"), ld, tm,
-                            Province.valueOf(resultset1.getString("PROVINCE")), Genre.valueOf(resultset1.getString("GENRE")),
-                            resultset1.getInt("MAX_NUM_SEATS"), resultset1.getInt("NUM_SEATS_TYPE"), seatsremaining, ticketSoldNumberForType, price,
-                            manager, resultset1.getString("ARTISTS"), resultset1.getString("DESCRIPTION_"), resultset1.getInt("NUM_ARTISTS"), bl);
-                    result.add(currentFestival);
-                    break;
-                }
-                case 2 -> {
-                    Theater currentTheatre = new Theater(resultset1.getInt("ID_EVENT"), resultset1.getString("NAME_"),
-                            resultset1.getString("CITY"), resultset1.getString("LOCATION"), ld, tm,
-                            Province.valueOf(resultset1.getString("PROVINCE")), Genre.valueOf(resultset1.getString("GENRE")),
-                            resultset1.getInt("MAX_NUM_SEATS"), resultset1.getInt("NUM_SEATS_TYPE"), seatsremaining, ticketSoldNumberForType, price,
-                            manager, resultset1.getString("ARTISTS"), resultset1.getString("DESCRIPTION_"), resultset1.getString("AUTHOR"), bl);
-                    result.add(currentTheatre);
-                    break;
-                }
-                case 3 -> {
-                    Other currentOther = new Other(resultset1.getInt("ID_EVENT"), resultset1.getString("NAME_"),
-                            resultset1.getString("CITY"), resultset1.getString("LOCATION"), ld, tm,
-                            Province.valueOf(resultset1.getString("PROVINCE")), Genre.valueOf(resultset1.getString("GENRE")),
-                            resultset1.getInt("MAX_NUM_SEATS"), resultset1.getInt("NUM_SEATS_TYPE"), seatsremaining, ticketSoldNumberForType, price,
-                            manager, resultset1.getString("ARTISTS"), resultset1.getString("DESCRIPTION_"), bl);
-                    result.add(currentOther);
-                    break;
+                double[] price = {resultset1.getDouble("BASE_PRICE"), resultset1.getDouble("PREMIUM_PRICE"), resultset1.getDouble("VIP_PRICE")};
+                int[] seatsremaining = {resultset1.getInt("REMAINING_BASE_SEATS"), resultset1.getInt("REMAINING_PREMIUM_SEATS"), resultset1.getInt("REMAINING_VIP_SEATS")};
+                int[] ticketSoldNumberForType = {resultset1.getInt("SOLD_BASE_SEATS"), resultset1.getInt("SOLD_PREMIUM_SEATS"), resultset1.getInt("SOLD_VIP_SEATS")};
+                switch (resultset1.getInt("TYPE")) {
+                    case 0 -> {
+                        Concert currentConcert = new Concert(resultset1.getInt("ID_EVENT"), resultset1.getString("NAME_"),
+                                resultset1.getString("CITY"), resultset1.getString("LOCATION"), ld, tm,
+                                Province.valueOf(resultset1.getString("PROVINCE")), Genre.valueOf(resultset1.getString("GENRE")),
+                                resultset1.getInt("MAX_NUM_SEATS"), resultset1.getInt("NUM_SEATS_TYPE"), seatsremaining, ticketSoldNumberForType, price,
+                                manager, resultset1.getString("ARTISTS"), resultset1.getString("DESCRIPTION_"), bl);
+                        result.add(currentConcert);
+                    }
+                    case 1 -> {
+                        Festival currentFestival = new Festival(resultset1.getInt("ID_EVENT"), resultset1.getString("NAME_"),
+                                resultset1.getString("CITY"), resultset1.getString("LOCATION"), ld, tm,
+                                Province.valueOf(resultset1.getString("PROVINCE")), Genre.valueOf(resultset1.getString("GENRE")),
+                                resultset1.getInt("MAX_NUM_SEATS"), resultset1.getInt("NUM_SEATS_TYPE"), seatsremaining, ticketSoldNumberForType, price,
+                                manager, resultset1.getString("ARTISTS"), resultset1.getString("DESCRIPTION_"), countWords(resultset1.getString("ARTISTS")), bl);
+                        result.add(currentFestival);
+                    }
+                    case 2 -> {
+                        Theater currentTheatre = new Theater(resultset1.getInt("ID_EVENT"), resultset1.getString("NAME_"),
+                                resultset1.getString("CITY"), resultset1.getString("LOCATION"), ld, tm,
+                                Province.valueOf(resultset1.getString("PROVINCE")), Genre.valueOf(resultset1.getString("GENRE")),
+                                resultset1.getInt("MAX_NUM_SEATS"), resultset1.getInt("NUM_SEATS_TYPE"), seatsremaining, ticketSoldNumberForType, price,
+                                manager, resultset1.getString("ARTISTS"), resultset1.getString("DESCRIPTION_"), resultset1.getString("AUTHOR"), bl);
+                        result.add(currentTheatre);
+                    }
+                    case 3 -> {
+                        Other currentOther = new Other(resultset1.getInt("ID_EVENT"), resultset1.getString("NAME_"),
+                                resultset1.getString("CITY"), resultset1.getString("LOCATION"), ld, tm,
+                                Province.valueOf(resultset1.getString("PROVINCE")), Genre.valueOf(resultset1.getString("GENRE")),
+                                resultset1.getInt("MAX_NUM_SEATS"), resultset1.getInt("NUM_SEATS_TYPE"), seatsremaining, ticketSoldNumberForType, price,
+                                manager, resultset1.getString("ARTISTS"), resultset1.getString("DESCRIPTION_"), bl);
+                        result.add(currentOther);
+                    }
                 }
             }
+        } catch (SQLException e){
+            throw new RuntimeException("Errore nella query dell'evento (ResearchDAO)");
         }
-    } catch (Exception e){
-        System.out.println("Hai swaggato troppo, chiudo e riapro");
-    }
+        ConnectionDB.closeConnection(conn); // chiudo la connessione
         return result;
-    } // Ritorna tutti gli eventi
+    } // Quando sulla ResearchBar non ho nulla, allora restituisco tutti gli eventi
+    @Override
+    public ArrayList<Event> getFilteredEvents(String search) throws SQLException{ // Quando qualcuno scrive sulla ResearchBar (TextField) e usa o meno i filtri, allora uso questo metodo
+
+        return null;
+    }
 
     private ArrayList<String> splitStringToArrayList(String s){
         String[] arrayString = s.split(",");
@@ -145,4 +127,19 @@ public class ResearchDAO implements IResearchDAO{
         return gr;
     }
 
+    private int countWords(String input){ // mi serve per contare quanti artisti sono stati messi nel festival, questo lo faccio separando la stringa
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        // Rimuove eventuali spazi bianchi prima e dopo la stringa
+        input = input.trim();
+
+        // Se la stringa è vuota dopo il trim, ritorna 0
+        if (input.isEmpty()) {
+            return 0;
+        }
+        // Divide la stringa in base alla virgola
+        String[] words = input.split("\\s*,\\s*");
+        return words.length;
+    }
 }
