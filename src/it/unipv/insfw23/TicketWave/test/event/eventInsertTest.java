84b13a108cee262d1b46f4a008df82d34d325f95
Davide-h <davide.colli03@universitadipavia.it>
2024-06-16 23:51:29 +0200

    Test Login & SignUp

---
+++ src/it/unipv/insfw23/TicketWave/test/event/eventInsertTest.java
@@ -0,0 +1,9 @@
+package it.unipv.insfw23.TicketWave.test.event;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+public class eventInsertTest {
+
+}
