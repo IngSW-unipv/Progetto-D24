0cca437d424f2168d7e0a9056d4da77eb546c1d7
lollo-delfo <loris.delfini01@universitadipavia.it>
2024-06-17 16:18:17 +0200

    sistemazione classi e javadoc notifiche

--- src/it/unipv/insfw23/TicketWave/modelDomain/notifications/Notification.java
+++ src/it/unipv/insfw23/TicketWave/modelDomain/notifications/Notification.java
@@ -6,60 +6,94 @@ import java.time.temporal.ChronoUnit;
 
 import it.unipv.insfw23.TicketWave.modelDomain.user.*;
 
+/**
+ * This class represents a notification, an object whose function is to deliver messages to {@link User}s
+ */
+
 public class Notification {
-		int id;
-		String emailReceiver;
-		String msg;
-		LocalTime time;
-		LocalDate date;
-		
-		public Notification(int id, String emailReceiver, String msg) {
-			this.id = id;
-			this.emailReceiver = emailReceiver;
-			this.msg = msg;
-			date = LocalDate.now();
-			time = LocalTime.now().truncatedTo(ChronoUnit.SECONDS);
-		}
+	int id;
+	String emailReceiver;
+	String msg;
+	LocalTime time;
+	LocalDate date;
+	
+	/**
+	 * This constructor associates the notification's data provided by the {@link NotificationHandler} with the creation date and time 
+	 * @param id the identifier number of the created notification
+	 * @param emailReceiver a {@link String} that allows to trace back to the notification's recipient {@link User}
+	 * @param msg a {@link String} that represents the real message to communicate
+	 */	
+	public Notification(int id, String emailReceiver, String msg) {
+		this.id = id;
+		this.emailReceiver = emailReceiver;
+		this.msg = msg;
+		date = LocalDate.now();
+		time = LocalTime.now().truncatedTo(ChronoUnit.SECONDS);
+	}
+	
+	/**
+	 * returns the {@link Integer} number that identifies a notification
+	 * @return the notification id
+	 */
+	public int getId() {
+		return id;
+	}
 
-		public int getId() {
-			return id;
-		}
+	/**
+	 * returns a {@link String} to identify the notification recipient 
+	 * @return the notification receiver in a {@link String}
+	 */
+	public String getEmailReceiver() {
+		return emailReceiver;
+	}
+	
+	/**
+	 * provides the way to set who the notification is for
+	 * @param email a {@link String} to identify the notification recipient
+	 */
+	public void setEmailReceiver(String email) {
+		emailReceiver = email;
+	}
 
-		public void setId(int id) {
-			this.id = id;
-		}
-		
-		public String getEmailReceiver() {
-			return emailReceiver;
-		}
-		
-		public void setEmailReceiver(String email) {
-			emailReceiver = email;
-		}
+	/**
+	 * returns the notification message   
+	 * @return the notification message as a {@link String}
+	 */
+	public String getMsg() {
+		return msg;
+	}
 
-		public String getMsg() {
-			return msg;
-		}
+	/**
+	 * returns the notification generation time
+	 * @return a {@link LocalTime} that indicates the notification time
+	 */
+	public LocalTime getTime() {
+		return time;
+	}
 
-		public void setMsg(String msg) {
-			this.msg = msg;
-		}
+	/**
+	 * provides the way to set the notification generation time
+	 * @param time a {@link LocalTime} value 
+	 */
+	public void setTime(LocalTime time) {
+		this.time = time;
+	}
 
-		public LocalTime getTime() {
-			return time;
-		}
+	/**
+	 * returns the notification generation date
+	 * @return a {@link LocalDate} that indicates the notification date
+	 */
+	public LocalDate getDate() {
+		return date;
+	}
 
-		public void setTime(LocalTime time) {
-			this.time = time;
-		}
-
-		public LocalDate getDate() {
-			return date;
-		}
-
-		public void setDate(LocalDate date) {
-			this.date = date;
-		}
+	/**
+	 * provides the way to set the notification generation date
+	 * @param date a {@link LocalDate} value
+	 */
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
 		
 		
 		
