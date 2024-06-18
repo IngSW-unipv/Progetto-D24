d49626cff42a969027eccc057c8f963852a31508
xNichox <nicholas.schiavonravazz01@universitadipavia.it>
2024-06-18 19:33:32 +0200

    altre modifiche

--- src/it/unipv/insfw23/TicketWave/test/event/TestEvent.java
+++ src/it/unipv/insfw23/TicketWave/test/event/TestEvent.java
@@ -87,38 +87,6 @@ public class TestEvent {
         }
     }
 
-    // li lascio oppure no i test sotto a questo commento?
-    @Test
-    public void createNullLimitCaseConcert(){ // Le date non possono avere valore nullo
-        try {
-            // caso minimo / nullo
-            co = new Concert(0, null, null, null, null, null, null, null, 0, 0,
-                    null, null, null, null, null, null, null);
-            System.out.println(co);
-        } catch (IllegalArgumentException e) {
-            assertEquals("The date has a null value, enter a new date", e.getMessage());
-        }
-    }
-
-    @Test
-    public void createDataLimitCaseConcert(){ // Le date non possono né valori prima del 2000 né dopo il 2050
-        try {
-            int[] a = {999999999, 999999999, 999999999, 999999999};
-            int[] b = {999999999, 999999999, 999999999};
-            double[] p = {999999999, 999999999, 999999999};
-            // caso con stringhe lunghe e valori al limite, numero di sedute > 3 che sono quelle rappresentabili
-            co = new Concert(100000, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                    "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                    "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.",
-                    LocalDate.of(10000, 10, 30), LocalTime.parse("23:59:59"), Province.MILANO, Genre.EDM, 999999999,
-                    3, a, b, p, mg, "Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. ",
-                    "Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.", bl);
-            System.out.println(co);
-        } catch (IllegalArgumentException e) {
-            assertEquals("Please enter new date, invalid date", e.getMessage());
-        }
-    }
-
     @Test
     public void checkEventsCreatedFromManager(){
         try {
