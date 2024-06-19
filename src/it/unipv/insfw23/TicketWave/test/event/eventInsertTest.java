6542a33197d81ea45ec1ad8ac836f924c40626ab
lollo-delfo <loris.delfini01@universitadipavia.it>
2024-06-19 17:13:42 +0200

    fix test

--- src/it/unipv/insfw23/TicketWave/test/event/eventInsertTest.java
+++ src/it/unipv/insfw23/TicketWave/test/event/eventInsertTest.java
@@ -1,10 +1,16 @@
 package it.unipv.insfw23.TicketWave.test.event;
 
 import it.unipv.insfw23.TicketWave.dao.eventDao.EventDao;
+import it.unipv.insfw23.TicketWave.dao.profileDao.ProfileDao;
+import it.unipv.insfw23.TicketWave.exceptions.AccountAlreadyExistsException;
 import it.unipv.insfw23.TicketWave.modelDomain.event.*;
 import it.unipv.insfw23.TicketWave.modelDomain.user.Manager;
+import javafx.application.Platform;
 import javafx.scene.image.Image;
+
+import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.sql.SQLException;
@@ -16,38 +22,65 @@ import static org.junit.Assert.*;
 
 public class eventInsertTest {
     private EventDao eventDao;
+    private ProfileDao profileDao;
+    private Manager manager;
     private Event eventFestivalTest;
     private Event eventConcertTest;
     private Event eventTheatreTest;
     private Event eventOtherTest;
     private ArrayList<Event> events = new ArrayList<>();
     private int MAX_EVENTS_FOR_BASE_SUB = 5;
+    private boolean condition = true;
 
+    @BeforeClass
+    public static void setUp2() {
+    	Platform.startup(()->{});
+    }
+    
     @Before
-    public void setUp(){
+    public void setUp(){    	
         int[] remainedSeats = {20};
         int[] ticketSold = {2080};
         double[] price = {150};
+        
+//        System.out.println(condition);
+//        if(condition) {
+//        	System.out.println("stampa");
+//        	Platform.startup(()->{});
+//        	this.condition = false;
+//        	System.out.println(condition);
+//        }
+        
         Image photo = new Image("/it/unipv/insfw23/TicketWave/modelView/imagesResources/eventImageExample.jpg"); // da problemi questa riga (non posso mettere imagine null perchè non è previsto da db)
 
-        Manager manager = new Manager("Giorgio", "Mastrota", "1990-01-01", "giorgio1@example.com", "eminflex", Province.CATANIA, "1234567890123456",
+        manager = new Manager("Giorgio", "Mastrota", "1990-01-01", "giorgio1@example.com", "eminflex", Province.CATANIA, "1234567890123456",
                 events, MAX_EVENTS_FOR_BASE_SUB, 1, LocalDate.now(), 0);
 
-        eventFestivalTest = new Festival(35, "Nameless", "Como", "Parco di Como", LocalDate.of(2024, 4, 20), LocalTime.of(14, 4), Province.COMO, Genre.EDM,
+        eventFestivalTest = new Festival(Short.MAX_VALUE-1, "Nameless", "Como", "Parco di Como", LocalDate.of(2024, 4, 20), LocalTime.of(14, 4), Province.COMO, Genre.EDM,
                 3000, 1, remainedSeats, ticketSold, price, manager, "Rooler, Salmo, Nello Taver", "Festival di musica EDM", photo);
 
-        eventConcertTest = new Concert(1, "Martin Garrix", "Milano", "San Siro", LocalDate.of(2024, 7, 24), LocalTime.of(21, 34), Province.MILANO, Genre.EDM, 2000,
-                2, remainedSeats, ticketSold, price, manager, "Martin Garrix", "Concerto di Martin Garrix", photo);
+        eventConcertTest = new Concert(Short.MAX_VALUE-2, "Martin Garrix", "Milano", "San Siro", LocalDate.of(2024, 7, 24), LocalTime.of(21, 34), Province.MILANO, Genre.EDM, 2000,
+                1, remainedSeats, ticketSold, price, manager, "Martin Garrix", "Concerto di Martin Garrix", photo);
 
-        eventTheatreTest = new Theater(2, "Franchino er Criminale", "Roma", "Teatro de Tivoli", LocalDate.of(2023, 10, 30), LocalTime.of(22, 50), Province.ROMA, Genre.COMMEDIA,
-                1200, 3, remainedSeats, ticketSold, price, manager, "Franchino er criminale", "Commedia di Franchino er criminale ", "Paolo", photo);
+        eventTheatreTest = new Theater(Short.MAX_VALUE-3, "Franchino er Criminale", "Roma", "Teatro de Tivoli", LocalDate.of(2023, 10, 30), LocalTime.of(22, 50), Province.ROMA, Genre.COMMEDIA,
+                1200, 1, remainedSeats, ticketSold, price, manager, "Franchino er criminale", "Commedia di Franchino er criminale ", "Paolo", photo);
 
-        eventOtherTest = new Other(3, "Sagra della salsiccia", "Roma", "Mercato de Roma", LocalDate.of(2023, 7, 22), LocalTime.parse("19:00:00"), Province.ROMA, null, 0,
-                0, remainedSeats, ticketSold, price, manager, null, "Sagra della salsiccia de Roma, er mejo", photo);
+        eventOtherTest = new Other(Short.MAX_VALUE-4, "Sagra della salsiccia", "Roma", "Mercato de Roma", LocalDate.of(2023, 7, 22), LocalTime.parse("19:00:00"), Province.ROMA, Genre.OTHER, 0,
+                1, remainedSeats, ticketSold, price, manager, null, "Sagra della salsiccia de Roma, er mejo", photo);
     }
+    
 
     @Test
     public void insertionTest1(){
+    	profileDao = new ProfileDao();
+    	try {
+			profileDao.insertManager(manager);
+		} catch(AccountAlreadyExistsException e) {
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+    	
         eventDao = new EventDao();
         boolean result = true;
 
@@ -63,6 +96,15 @@ public class eventInsertTest {
 
     @Test
     public void insertionTest2(){
+    	profileDao = new ProfileDao();
+    	try {
+			profileDao.insertManager(manager);
+		} catch(AccountAlreadyExistsException e) {
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+    	
         eventDao = new EventDao();
         boolean result = true;
 
@@ -78,6 +120,15 @@ public class eventInsertTest {
 
     @Test
     public void insertionTest3(){
+    	profileDao = new ProfileDao();
+    	try {
+			profileDao.insertManager(manager);
+		} catch(AccountAlreadyExistsException e) {
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+    	
         eventDao = new EventDao();
         boolean result = true;
 
@@ -94,6 +145,15 @@ public class eventInsertTest {
 
     @Test
     public void insertionTest4(){
+    	profileDao = new ProfileDao();
+    	try {
+			profileDao.insertManager(manager);
+		} catch(AccountAlreadyExistsException e) {
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+    	
         eventDao = new EventDao();
         boolean result = true;
 
